dfec98eea28d6cc396b1dbe62610fbb8b48eaa2a
c501306014 <c501306014@gmail.com>
2021-01-23 09:54:02 +0900

    @express APIたたいた時のエラー処理を追加

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios')
 
-
 const {
   response
 } = require('express');
@@ -14,22 +13,36 @@ router.get('/', function (req, res, next) {
   });
 });
 
-
 router.get('/test', function (req, res, next) {
-  function error_res(place, err) {
-    // console.log('error at ' + place)
-    res.render('test', {
-      state: 'error',
-      message: err
-    })
-  }
-
   const test = async function (station_name, pref_name) {
-
     station_name = encodeURI(station_name)
     pref_name = encodeURI(pref_name)
-    let X, Y, RailURL = `http://express.heartrails.com/api/json?method=getStations&name=${station_name}&prefecture=${pref_name}`
-    const RailRes = await axios.get(RailURL).catch((err) => error_res('rail', err))
+    let X = 0
+    let Y = 0
+    const RailURL = `http://express.heartrails.com/api/json?method=getStations&name=${station_name}&prefecture=${pref_name}`
+    const RailRes = await axios
+      .get(RailURL)
+      .catch(err => {
+        return err.response
+      })
+
+    if('error' in RailRes.data['response']){
+      console.log(RailRes.data['response']["error"])
+      
+    }
+    else if (!RailRes.data['response']['station'].length) {
+      const err = new Error('駅名と都道府県の組み合わせが正しくありません')
+      res.status(400).send({
+        error: err.message,
+        data :RailRes.data['response']
+      })
+    }
+    else{
+      console.log("---------------------------------------------------")
+    }
+
+
+
     X = RailRes.data['response']['station'][0]['x']
     Y = RailRes.data['response']['station'][0]['y']
 
@@ -39,24 +52,16 @@ router.get('/test', function (req, res, next) {
     const hotpepperRes = await axios.get(hotpepperURL).catch((err) => error_res('hotpepper', err))
     let shop_list = hotpepperRes["data"]["results"]["shop"]
 
-    shop_list.forEach(shop_data => {
-      // console.log([shop_data["name"], shop_data["address"], shop_data["urls"]["pc"], shop_data["budget"]["average"], ])
-    });
-
-    // res.render('test', {
     res.send({
       state: 'success',
       shop_list: shop_list
     })
   }
 
-  
-  console.log(req.query)
   function main() {
     test(req.query.station_name, req.query.pref_name)
   }
   main()
-
 })
 
 module.exports = router;
