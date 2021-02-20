b52dc123086279f355eca9dbdc7b402e30c3ec32
c501306014 <c501306014@gmail.com>
2021-02-20 18:24:46 +0900

    都道府県選択機能をコンポーネント化

--- vue-app/src/views/page1.vue
+++ vue-app/src/views/page1.vue
@@ -3,19 +3,7 @@
     <div>
       <b-form @submit="onSubmit">
         <b-form-row>
-          <b-form-group
-            id="input-pref"
-            class="col-md-6"
-            label="都道府県名"
-            label-for="pref-name"
-          >
-            <b-form-select
-              id="pref-name"
-              v-model="form.pref"
-              :options="prefectures"
-              required
-            ></b-form-select>
-          </b-form-group>
+          <selectPrefecture v-model="form.pref" />
           <b-form-group
             id="input-station"
             class="col-md-6"
@@ -35,6 +23,8 @@
         <b-button type="submit" variant="primary">Submit</b-button>
       </b-form>
     </div>
+
+    <p>pref: {{ form.pref }}</p>
 
     <div v-if="shop_list != null">
       <ul>
@@ -79,9 +69,13 @@
 
 <script>
 import axios from "axios";
+import selectPrefecture from "@/components/prefecture.vue";
 
 export default {
   name: "page1",
+  components: {
+    selectPrefecture,
+  },
   data() {
     return {
       shop_list: null,
@@ -94,63 +88,13 @@ export default {
         pref: null,
         station: null,
       },
-      prefectures: [
-        { text: "都道府県を選択してください", value: null, disabled: true },
-        "北海道",
-        "青森県",
-        "岩手県",
-        "宮城県",
-        "秋田県",
-        "山形県",
-        "福島県",
-        "茨城県",
-        "栃木県",
-        "群馬県",
-        "埼玉県",
-        "千葉県",
-        "東京都",
-        "神奈川県",
-        "新潟県",
-        "富山県",
-        "石川県",
-        "福井県",
-        "山梨県",
-        "長野県",
-        "岐阜県",
-        "静岡県",
-        "愛知県",
-        "三重県",
-        "滋賀県",
-        "京都府",
-        "大阪府",
-        "兵庫県",
-        "奈良県",
-        "和歌山県",
-        "鳥取県",
-        "島根県",
-        "岡山県",
-        "広島県",
-        "山口県",
-        "徳島県",
-        "香川県",
-        "愛媛県",
-        "高知県",
-        "福岡県",
-        "佐賀県",
-        "長崎県",
-        "熊本県",
-        "大分県",
-        "宮崎県",
-        "鹿児島県",
-        "沖縄県",
-      ],
     };
   },
   methods: {
     async onSubmit(event) {
       event.preventDefault();
-      const url = "https://izakaya-search.herokuapp.com/test";
-      // const url = "http://localhost:3000/test";
+      // const url = "https://izakaya-search.herokuapp.com/test";
+      const url = "http://localhost:3000/test";
       const response = await axios
         .get(url, {
           params: {
@@ -166,9 +110,6 @@ export default {
       this.shop_list = response.data.shop_list;
       console.log(this.shop_list);
       this.rows = this.shop_list.length;
-    },
-    onReset(event) {
-      event.preventDefault();
     },
   },
 };
