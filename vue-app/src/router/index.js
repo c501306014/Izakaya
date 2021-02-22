51b5847cd4d72eff262f4d7ec5ba03817fd5ae83
c501306014 <c501306014@gmail.com>
2021-02-22 14:36:21 +0900

    page1.vueのリネームに伴う変更

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import page1 from '@/views/page1.vue'
+import search from '@/views/search.vue'
 
 
 Vue.use(Router)
@@ -10,7 +10,7 @@ export default new Router({
   mode: 'history',
   routes: [{
       path: '/',
-      component: page1
+      component: search
     },
   ]
 })
