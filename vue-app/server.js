18cf217f53719979ebf2be4b9229258451935e1d
c501306014 <c501306014@gmail.com>
2021-02-26 11:06:48 +0900

    リファクタリング

--- vue-app/server.js
+++ vue-app/server.js
@@ -15,32 +15,31 @@ app.use(express.urlencoded({
 }));
 
 const port = process.env.PORT || 3000;
-
 app.listen(port);
 console.log('server started ' + port);
 
+
 app.get('/search', function (req, res, next) {
-  function main() {
-    search_bar( req.query );
-  }
   const search_bar = async function (stationPos) {
-    // start 店のリストを取得
+    // request to Hotpepper API
     let hotpepperAPI_key = "dd28cf82f92a25dc";
     let hotpepperURL = `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${hotpepperAPI_key}&lat=${stationPos.Y}&lng=${stationPos.X}&free_drink=1&private_room=1&course=1range=2&count=100&order=1&format=json`;
-
     const hotpepperRes = await axios
       .get(hotpepperURL)
       .catch(err => {
         return res.status(400).send(err.response);
       });
-
-    let shop_list = hotpepperRes["data"]["results"]["shop"];
+    
+    // response to frontend
+    const shop_list = hotpepperRes["data"]["results"]["shop"];
     res.send({
       shop_list: shop_list
     });
-    // end 店のリストを取得
-    console.log("3");
   }
-  
+
+  // main function
+  function main() {
+    search_bar(req.query);
+  }
   main();
 })
