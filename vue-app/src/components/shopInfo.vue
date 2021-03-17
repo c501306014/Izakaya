7dcc2dcb89e4325705bbc84d7882e193b2b958a8
c501306014 <c501306014@gmail.com>
2021-03-17 21:08:53 +0900

    居酒屋一覧のアイテムをコンポーネント化

---
+++ vue-app/src/components/shopInfo.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <a class="shop_img" :href="shop_data.urls.pc">
+      <img :src="shop_data.photo.pc.l" alt="" />
+    </a>
+    <a class="shop_link" :href="shop_data.urls.pc">
+      {{ shop_data.name }}
+    </a>
+    <span class="shop_info_head">カテゴリ</span>
+    <span class="shop_info_content">
+      {{ shop_data.genre.name }}
+    </span>
+    <span class="shop_info_head">予算</span>
+    <span class="shop_info_content">
+      {{ shop_data.budget.name }}
+    </span>
+    <span class="shop_info_head">最大人数</span>
+    <span class="shop_info_content"> {{ shop_data.party_capacity }}人 </span>
+    <span class="shop_info_head">コース</span>
+    <span class="shop_info_content">
+      <span
+        class="course-info bg-warning"
+        v-show="shop_data.free_drink.match(/あり/)"
+      >
+        飲み放題有
+      </span>
+      <span
+        class="course-info bg-warning"
+        variant="warning"
+        v-show="shop_data.free_food.match(/あり/)"
+        >食べ放題有</span
+      >
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "shopInfo",
+  props: ["shop_data"],
+};
+</script>
+
+<style scoped>
+.shop_img {
+  min-height: 24rem;
+  grid-column: 1/4;
+}
+
+.shop_img img {
+  border-radius: 5%;
+  filter: drop-shadow(1px 1px 5px rgba(0, 0, 0, 0.3));
+  transition: 0.2s;
+}
+
+.shop_img img:hover {
+  transform: scale(1.05, 1.05);
+}
+
+.shop_link {
+  grid-column: 1/4;
+  margin-top: 1rem;
+}
+
+.shop_info_head {
+  grid-column: 2/3;
+  text-align: left;
+}
+.shop_info_content {
+  text-align: left;
+}
+
+@media screen and (max-width: 746px) {
+  .shop_img {
+    min-height: auto;
+  }
+}
+</style>
--- vue-app/src/views/result.vue
+++ vue-app/src/views/result.vue
@@ -72,41 +72,7 @@
                 v-for="shop_data in paginated('paginate-items')"
                 :key="shop_data.name"
               >
-                <div>
-                  <a class="shop_img" :href="shop_data.urls.pc">
-                    <img :src="shop_data.photo.pc.l" alt="" />
-                  </a>
-                  <a class="shop_link" :href="shop_data.urls.pc">
-                    {{ shop_data.name }}
-                  </a>
-                  <span class="shop_info_head">カテゴリ</span>
-                  <span class="shop_info_content">
-                    {{ shop_data.genre.name }}
-                  </span>
-                  <span class="shop_info_head">予算</span>
-                  <span class="shop_info_content">
-                    {{ shop_data.budget.name }}
-                  </span>
-                  <span class="shop_info_head">最大人数</span>
-                  <span class="shop_info_content">
-                    {{ shop_data.party_capacity }}人
-                  </span>
-                  <span class="shop_info_head">コース</span>
-                  <span class="shop_info_content">
-                    <span
-                      class="course-info bg-warning"
-                      v-show="shop_data.free_drink.match(/あり/)"
-                    >
-                      飲み放題有
-                    </span>
-                    <span
-                      class="course-info bg-warning"
-                      variant="warning"
-                      v-show="shop_data.free_food.match(/あり/)"
-                      >食べ放題有</span
-                    >
-                  </span>
-                </div>
+                <shopInfo :shop_data="shop_data" />
               </li>
             </div>
           </paginate>
@@ -135,11 +101,13 @@
 
 <script>
 import scrollToTop from "@/components/scrollToTop.vue";
+import shopInfo from "@/components/shopInfo.vue";
 
 export default {
   name: "result",
   components: {
     scrollToTop,
+    shopInfo,
   },
   data() {
     return {
@@ -271,34 +239,6 @@ li {
   width: 32rem;
 }
 
-.shop_img {
-  min-height: 24rem;
-  grid-column: 1/4;
-}
-
-.shop_img img {
-  border-radius: 5%;
-  filter: drop-shadow(1px 1px 5px rgba(0, 0, 0, 0.3));
-  transition: 0.2s;
-}
-
-.shop_img img:hover {
-  transform: scale(1.05, 1.05);
-}
-
-.shop_link {
-  grid-column: 1/4;
-  margin-top: 1rem;
-}
-
-.shop_info_head {
-  grid-column: 2/3;
-  text-align: left;
-}
-.shop_info_content {
-  text-align: left;
-}
-
 .buttons {
   max-width: 50rem;
   margin: 1rem auto;
@@ -325,11 +265,5 @@ li {
 }
 .course-info:hover {
   cursor: default;
-}
-
-@media screen and (max-width: 746px) {
-  .shop_img {
-    min-height: auto;
-  }
 }
 </style>
