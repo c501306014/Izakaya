3a95ceb11d96c23a950d28a2be0bc03956a0a0fd
c501306014 <c501306014@gmail.com>
2021-03-19 20:41:27 +0900

    vuex-persistedstateを導入、ページリロードなどしても状態を維持できるように改善

--- vue-app/src/store/index.js
+++ vue-app/src/store/index.js
@@ -1,22 +1,28 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
+
+const initialState = {
+  form: {
+    pref: "",
+    station: ""
+  },
+  search_start: 1,
+  shop_list: null,
+  filtered_shop_list: null,
+  err_message: null,
+  // server_url: "https://izakaya-search.herokuapp.com/search",
+  server_url:   "http://localhost:3000/search",
+}
+
+
+
 // create store
 export default new Vuex.Store({
-  state: {
-    form: {
-      pref: "",
-      station: ""
-    },
-    search_start: 1,
-    shop_list: null,
-    filtered_shop_list: null,
-    err_message: null,
-    server_url: "https://izakaya-search.herokuapp.com/search",
-    // const url = "http://localhost:3000/search";
-  },
+  state: initialState,
   mutations: {
     setForm(state, form) {
       state.form = form;
@@ -33,5 +39,6 @@ export default new Vuex.Store({
     setErrMessage(state, err_message) {
       state.err_message = err_message;
     }
-  }
+  },
+  plugins:[createPersistedState()]
 });
